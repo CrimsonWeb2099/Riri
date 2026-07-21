0cad1f4ed0de73245c8773dc301c2dde82d40ec3
CrimsonWeb2099 <taeva714@gmail.com>
2026-07-21 15:01:24 -0500

    Refactor birthday message display and content handling

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const enter = document.getElementById("enter");
 const loading = document.querySelector(".loading-screen");
-const letter = document.getElementById("letter");
+const content = document.getElementById("content");
 const text = document.getElementById("letter-text");
 
 
 const message = `
-¡Feliz cumpleaños, Riri! 
+¡Feliz cumpleaños, Riri! 🖤
 
 Espero que hayas tenido un día hermoso,
 lleno de felicidad, sonrisas y momentos especiales.
@@ -16,23 +16,18 @@ en este nuevo año de tu vida.
 Que todos tus sueños se cumplan
 y que nunca te falten razones para sonreír.
 
-Eres una persona increíble
-y espero que nunca olvides lo especial que eres.
+Eres una persona increíble.
 
-Gracias por ser una amiga tan linda.
-
-Te deseo lo mejor siempre.
-
-
+🖤
 `;
 
 
 enter.onclick = () => {
 
-    loading.style.display = "none";
-    letter.style.display = "block";
+loading.style.display = "none";
+content.style.display = "block";
 
-    typeWriter();
+typeWriter();
 
 };
 
@@ -41,13 +36,13 @@ let i = 0;
 
 function typeWriter(){
 
-    if(i < message.length){
+if(i < message.length){
 
-        text.innerHTML += message.charAt(i);
-        i++;
+text.innerHTML += message.charAt(i);
+i++;
 
-        setTimeout(typeWriter, 45);
+setTimeout(typeWriter,45);
 
-    }
+}
 
 }
