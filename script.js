e107666a886c8cac2c693d75100eb561cf4a6867
CrimsonWeb2099 <taeva714@gmail.com>
2026-07-21 12:38:08 -0500

    Add click event to show letter and hide loading screen

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const enter = document.getElementById("enter");
+const loading = document.querySelector(".loading-screen");
+const letter = document.getElementById("letter");
 
+
+enter.onclick = () => {
+
+    loading.style.display = "none";
+    letter.style.display = "block";
+
+};
